01a0b3de4c8657687a9f527bc2880b8b974ba30d
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-11-05 19:50:33 +0700

    Menyelesaikan Halamn satu

--- app/src/main/java/com/example/navigasipage/HalamanSatu.kt
+++ app/src/main/java/com/example/navigasipage/HalamanSatu.kt
@@ -3,9 +3,19 @@ package com.example.navigasipage
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,7 +24,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanSatu(
     pilihanRasa: List<String>,
@@ -48,9 +62,67 @@ fun HalamanSatu(
                 ) {
                     RadioButton(
                         selected = rasaYgDipilih == item,
-                        onClick = { /*TODO*/ })
+                        onClick = {
+                            rasaYgDipilih = item
+                            onSelectionChange(item)
+                        }
+                    )
+                    Text(item)
                 }
-
+            }
+            Divider(
+                thickness = dimensionResource(id = R.dimen.thickness_divider),
+                modifier = Modifier.padding(bottom =
+                dimensionResource(id = R.dimen.padding_medium))
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(id = R.dimen.padding_medium))
+                    .weight(1f, false),
+                horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            ) {
+                OutlinedTextField(
+                    value = textJmlBeli,
+                    singleLine = true,
+                    shape = MaterialTheme.shapes.large,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    modifier = Modifier.width(150.dp),
+                    label = { Text(text = "Jumlah Order")},
+                    onValueChange = {textJmlBeli = it}
+                )
+                Button(
+                    modifier = Modifier.weight(1f),
+                    enabled = textJmlBeli.isNotEmpty(),
+                    onClick = { onConfirmButtonClicked(textJmlBeli.toInt())}
+                ) {
+                    Text(stringResource(R.string.confirm))
+                }
+            }
+            Divider(
+                thickness = dimensionResource(id = R.dimen.thickness_divider),
+                modifier = Modifier.padding(bottom =
+                dimensionResource(id = R.dimen.padding_medium))
+            )
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(id = R.dimen.padding_medium))
+                    .weight(1f, false),
+                horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+                verticalAlignment = Alignment.Bottom,
+            ){
+                OutlinedButton(modifier = Modifier.weight(1f),
+                    onClick = { onCanceledClicked}) {
+                    Text(stringResource(id = R.string.cancel))
+                }
+                Button(
+                    modifier = Modifier.weight(1f),
+                    enabled = textJmlBeli.isNotEmpty(),
+                    onClick = { onCanceledClicked}
+                ) {
+                    Text(stringResource(R.string.next))
+                }
             }
         }
     }
