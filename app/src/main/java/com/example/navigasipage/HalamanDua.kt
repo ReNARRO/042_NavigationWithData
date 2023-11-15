289f434dd9a0804c655938786dad389b437ffa30
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2023-11-15 20:24:42 +0700

    Menambhakan code untuk memanggil nama, nohp, alamat, yang sudah di tambahkan pada halaman form

--- app/src/main/java/com/example/navigasipage/HalamanDua.kt
+++ app/src/main/java/com/example/navigasipage/HalamanDua.kt
@@ -1,5 +1,7 @@
 package com.example.navigasipage
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,9 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.navigasipage.data.OrderUIState
 import com.example.navigasipage.ui.komponen.FormatTabelHarga
 
@@ -38,6 +42,46 @@ fun HalamanDua(
             modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
         ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                Text(stringResource(id = R.string.nama))
+                Text(text = orderUiState.nama)
+            }
+            Divider()
+            Spacer(modifier = Modifier.padding(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                Text(stringResource(id = R.string.nohp))
+                Text(text = orderUiState.notelp)
+            }
+            Divider()
+            Spacer(modifier = Modifier.padding(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                Text(stringResource(id = R.string.alamat))
+                Text(text = orderUiState.alamat)
+            }
+            Divider()
+            Spacer(modifier = Modifier.padding(16.dp))
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+            ){
+                Text(
+                    text = "PESANAN ANDA", fontWeight = FontWeight.Bold, modifier = Modifier
+                        .border(
+                            BorderStroke(2.dp, Color.Black)
+                        )
+                        .padding(10.dp)
+                )
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
             items.forEach { item ->
                 Column {
                     Text(item.first.uppercase())
